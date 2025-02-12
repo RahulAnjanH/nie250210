a5ae40730a54e7d5787d64bf2ef78fbd3d827757
Rahul <rahulanjanh@gmail.com>
2025-02-12 11:31:41 +0530

    some editing done

--- ars/02client_design/client/src/flights/FlightCreate.jsx
+++ ars/02client_design/client/src/flights/FlightCreate.jsx
@@ -8,27 +8,27 @@ function FlightCreate() {
             <div className="container">
                 <div className=" form-group mb-3">
                     <label htmlFor="flight number" className="form-label">Flight Number</label>
-                    <input type="email" className="form-control" id="flight number" placeholder="Please Enter fight number"/>
+                    <input type="text" className="form-control" id="flight number" placeholder="Please Enter fight number"/>
                 </div>
                 <div className=" form-group mb-3">
                     <label htmlFor="airline name" className="form-label">Airline name</label>
-                    <input type="email" className="form-control" id="airline name" placeholder="Please Enter Airline name"/>
+                    <input type="text" className="form-control" id="airline name" placeholder="Please Enter Airline name"/>
                 </div>
                 <div className=" form-group mb-3">
                     <label htmlFor="source" className="form-label">Source</label>
-                    <input type="email" className="form-control" id="source" placeholder="Please Enter source name"/>
+                    <input type="text" className="form-control" id="source" placeholder="Please Enter source name"/>
                 </div>
                 <div className=" form-group mb-3">
                     <label htmlFor="destation" className="form-label">Destination</label>
-                    <input type="email" className="form-control" id="destation" placeholder="Please Enter destination name"/>
+                    <input type="text" className="form-control" id="destation" placeholder="Please Enter destination name"/>
                 </div>
                 <div className=" form-group mb-3">
-                    <label htmlFor="flight capacity" className="form-label">Flight capacity</label>
-                    <input type="email" className="form-control" id="flight capacity" placeholder="Please Enter fight capacity(number of seats)"/>
+                    <label htmlFor="flight capacity" className="form-label">Capacity(number of seats)</label>
+                    <input type="text" className="form-control" id="flight capacity" placeholder="Please Enter fight capacity(number of seats)"/>
                 </div>
                 <div className=" form-group mb-3">
-                    <label htmlFor="flight price" className="form-label">Flight price</label>
-                    <input type="email" className="form-control" id="flight price" placeholder="Please Enter fight ticket price"/>
+                    <label htmlFor="flight price" className="form-label">Ticket price</label>
+                    <input type="text" className="form-control" id="flight price" placeholder="Please Enter fight ticket price"/>
                 </div>
                 <button className="btn btn-success">Create flight</button>
             </div>
--- ars/02client_design/client/src/flights/FlightEdit.jsx
+++ ars/02client_design/client/src/flights/FlightEdit.jsx
@@ -8,27 +8,27 @@ function FlightEdit(){
     <div className="container">
         <div className=" form-group mb-3">
             <label htmlFor="flight number" className="form-label">Flight Number</label>
-            <div><input type="email" className="form-control" id="flight number" placeholder="??"/></div>
+            <div className="form-control" id="flight number" >???</div>
         </div>
         <div className=" form-group mb-3">
             <label htmlFor="airline name" className="form-label">Airline name</label>
-           <div><input type="email" className="form-control" id="airline name" placeholder="??"/></div>
+            <div className="form-control" id="airline name" >???</div>
         </div>
         <div className=" form-group mb-3">
             <label htmlFor="source" className="form-label">Source</label>
-           <div><input type="email" className="form-control" id="source" placeholder="??"/> </div>
+            <div className="form-control" id="source" >???</div>
         </div>
         <div className=" form-group mb-3">
             <label htmlFor="destation" className="form-label">Destination</label>
-           <div> <input type="email" className="form-control" id="destation" placeholder="??"/></div>
+            <div className="form-control" id="destation" >???</div>
         </div>
         <div className=" form-group mb-3">
             <label htmlFor="flight capacity" className="form-label">Flight capacity</label>
-            <div><input type="email" className="form-control" id="flight capacity" placeholder="??"/></div>
+            <div className="form-control" id="flight capacity" >???</div>
         </div>
         <div className=" form-group mb-3">
             <label htmlFor="flight price" className="form-label">Flight price</label>
-            <input type="email" className="form-control" id="flight price" placeholder="Please enter the fight price"/>
+            <input type="text" className="form-control" id="flight price" placeholder="Please enter the fight price"/>
         </div>
         <button className="btn btn-warning">Update price</button>
     </div>
